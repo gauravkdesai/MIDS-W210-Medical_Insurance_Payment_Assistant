0fa7925a501081cea1cd2fa7d9f86a43a186cd34
Ubuntu <ubuntu@ip-172-31-9-3.us-west-2.compute.internal>
2020-07-23 02:01:28 +0000

    Playing around with d3js

--- frontend/src/pages/D3JS2.js
+++ frontend/src/pages/D3JS2.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 var d3 = require("d3")
 
-const width = 600;
+const width = 300;
 const height = 100;
-const margin = {top: 10, right: 120, bottom: 10, left: 40};
+const margin = {top: 10, right: 40, bottom: 10, left: 40};
 const dy = width / 6;
 const dx = 10;
 
@@ -101,7 +101,7 @@ class D3JS2 extends Component {
                     .attr("dy", "0.31em")
                     .attr("x", d => d._children ? -6 : 6)
                     .attr("text-anchor", d => d._children ? "end" : "start")
-                    .text(d => d.data.name)
+                    .text(d => d.data.name + d.data.value)
                   .clone(true).lower()
                     .attr("stroke-linejoin", "round")
                     .attr("stroke-width", 3)
